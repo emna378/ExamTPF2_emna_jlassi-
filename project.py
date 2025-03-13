d13b6e33b8266a238b482ed30ab2cb628594505e
emna <emnajlassi234@gmail.com>
2025-03-13 10:28:48 +0100

    project.py commited with if et else

--- project.py
+++ project.py
@@ -1,6 +1,8 @@
 data[1,3,5] 
+  if data :
 
-print('la somme est :',som)
-print('le min est', min(data)) 
-print('le max est', max(data))
-
+     print('la somme est :',som)
+     print('le min est', min(data)) 
+     print('le max est', max(data))
+  else :
+      print('dossier vide') 
